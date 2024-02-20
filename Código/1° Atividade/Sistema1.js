d507dc76e7560b1ed491923d5f7994e5b75037d8
Vitor Simões <159032949+VitorSimoesDS@users.noreply.github.com>
2024-02-20 17:55:34 -0300

    CoffeCode

--- Código/1° Atividade/Sistema1.js
+++ Código/1° Atividade/Sistema1.js
@@ -3,5 +3,5 @@ let number = parseInt(prompt("Insira um número: "));
 if (number % 2 === 0) {
     console.log("O valor é par.");
 } else {
-    console.log("O valor é impar.")
+    console.log("O valor é impar.");
 }
