3df642c9b2bb3da3dee256272ac74f3a641284fa
Vitor Simões <159032949+VitorSimoesDS@users.noreply.github.com>
2024-02-20 17:57:09 -0300

    CoffeCode

--- Código/2° Atividade/Sistema2.js
+++ Código/2° Atividade/Sistema2.js
@@ -7,6 +7,6 @@ if (number1 > number2 && number1 > number3) {
 } else if (number2 > number1 && number2 > number3){
     console.log("O segundo número (" + number2 + ") é o maior.");
 }else{
-    console.log("O terceiro número (" + number3 + ") é o maior.")
+    console.log("O terceiro número (" + number3 + ") é o maior.");
 }
 
