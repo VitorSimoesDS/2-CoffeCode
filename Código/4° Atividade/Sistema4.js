c0bc2cbcf9c1223fec3f14e32ddf1f1846fc0f61
Vitor Simões <159032949+VitorSimoesDS@users.noreply.github.com>
2024-02-20 18:04:43 -0300

    CoffeCode

--- Código/4° Atividade/Sistema4.js
+++ Código/4° Atividade/Sistema4.js
@@ -1,1 +1,30 @@
+function diaDaSemana(x) {
+    switch(x) {
+        case 1:
+            console.log("Segunda-feira");
+            break;
+        case 2:
+            console.log("Terça-feira");
+            break;
+        case 3:
+            console.log("Quarta-feira");
+            break;
+        case 4:
+            console.log("Quinta-feira");
+            break;
+        case 5:
+            console.log("Sexta-feira");
+            break;
+        case 6:
+            console.log("Sábado");
+            break;
+        case 7:
+            console.log("Domingo");
+            break;
+        default:
+            console.log("Número inválido. Por favor, insira um número de 1 a 7.");
+    }
+}
 
+var number = parseInt(prompt("Digite um número de 1 a 7:"));
+diaDaSemana(number);
